09aee9d6f9bce39c1294c5b5225ba7c5230b2723
Jean-Fred <JeanFred@github>
2013-10-14 16:10:38 +0200

    Add 'Format' (ie dimension) processing
    Add processing methods to parse the 'Format' field
    and replace with {{Size}} template.
    Add unit tests too.

--- processors.py
+++ processors.py
@@ -6,3 +6,35 @@ __authors__ = 'User:Jean-Frédéric'
 
 import re
 
+
+def parse_format(field, old_field_value):
+    """Parse a foramt and return the parsed."""
+    new_value = parse_format_unwrapped(old_field_value)
+    return {field: new_value}
+
+
+def _clean_dim(dim):
+    """Clean a dimension-like string"""
+    return re.sub(r"\s?,\s?", '.', dim).strip()
+
+
+def parse_format_unwrapped(text):
+
+    def repl(m):
+        """Convert the pattern matchd in {{Size}}."""
+        unit = 'cm'
+        elements = m.groupdict()
+        l = filter(None, [elements[x] for x in sorted(elements.keys())])
+        s = '|'.join([_clean_dim(dim) for dim in l])
+        return " {{Size|%s|%s}}" % (unit, s)
+
+    format_pattern = re.compile(r"""
+        (?P<a>[\d,\.]+?)   # Digits, comma or dot, captured as group
+        \s*x\s*            # Whitespace, x, whitespace
+        (?P<b>[\d,\.]+?)   # Same
+        \s*$               # Whitespace until the end
+        """, re.X)
+
+    new_value = re.sub(format_pattern, repl, text)
+    return new_value
+
--- test/test_processors.py
+++ test/test_processors.py
@@ -1,1 +1,15 @@
 import unittest
+from processors import parse_format_unwrapped
+
+
+class TestProcessors(unittest.TestCase):
+
+    def test_parse_format(self):
+        values = [
+            ('8,5 x 10', ' {{Size|cm|8.5|10}}'),
+            ('13 x 18', ' {{Size|cm|13|18}}'),
+            ('4,5 x 5,5', ' {{Size|cm|4.5|5.5}}'),
+            ('7 x 9,5', ' {{Size|cm|7|9.5}}'),
+        ]
+        for value, expected in values:
+            self.assertEqual(parse_format_unwrapped(value), expected)
