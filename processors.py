b692a7147334cb6c44b017e5a3bbe471963f9cc5
Jean-Fred <JeanFred@github>
2013-10-19 21:13:36 +0200

    Add date-like processing
    Add processing methods which go through a given string,
    looking for date indications, and returning it as an {{Other date}}.
    Add unit tests for every covered case.

--- processors.py
+++ processors.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 """Parsers useful for Archives metadata."""
 
@@ -38,3 +38,175 @@ def parse_format_unwrapped(text):
     new_value = re.sub(format_pattern, repl, text)
     return new_value
 
+
+def look_for_date(field, old_field_value):
+    """Wrapper around look_for_date_unwrapped.
+
+    Retrieve the values found by look_for_date_unwrapped
+    Re-add the old_field_value to the dictionary
+    Add the date and the year if they were found
+
+    """
+    print "Considering"
+    (date, year) = look_for_date_unwrapped(old_field_value)
+
+    result = {field: old_field_value}
+    if date:
+        print "--> adding Date %s " % date
+        result['date'] = date
+    if year:
+        print "--> adding year %s " % year
+        result['year'] = year
+    return result
+
+
+def look_for_date_unwrapped(text):
+    """Look for a date in the given text.
+
+    Search a given string for a date pattern, using regular expressions.
+    Return the date (either using the ISO YYY-MM-DD format
+    or the {{Other date}} template) and the year.
+
+    """
+
+    monthList = {
+        u'janvier': 1, u'février': 2, u'mars': 3, u'avril': 4,
+        u'mai': 5, u'juin': 6, 'juillet': 7, u'août': 8,
+        u'septembre': 9, u'octobre': 10, u'novembre': 11, u'décembre': 12
+        }
+
+    fullDatePattern = re.compile("""
+        (?P<day>\d+?)           # Some digits
+        \s                      # Whitespace
+        (?P<month>[\w]+?)       # Some letters, captured as 'month'
+        \s                      # Whitespace
+        (?P<year>\d\d\d\d)      # Four digits, captured
+        """, re.UNICODE + re.X)
+    monthDatePattern = re.compile("""
+        (?P<month>\w\w\w[\w]+?)  # Some letters, captured as 'month'
+        \s                       # Whitespace
+        (?P<year>\d\d\d\d)       # Four digits, captured as 'year'
+        """, re.UNICODE + re.X)
+    circaYearPattern = re.compile("""
+        Vers                    # The 'Vers' word
+        \s*?                    # Maybe some whitespace
+        (?P<year>\d\d\d\d)      # Four digits, captured as 'year'
+        """, re.UNICODE + re.X)
+    circaDatePattern = re.compile("""
+        Vers                    # The 'Vers' word
+        \s                      # Whitespace
+        (?P<month>\w*?)         # Some letters, captured as 'month'
+        \s                      # Whitespace
+        (?P<year>\d\d\d\d)      # Four digits, captured as 'year'>
+        """, re.UNICODE + re.X)
+    betweenDatePattern = re.compile("""
+        Entre                   # The 'Entre' word
+        [\s\w]*?                # Whatever words and whitespace
+        \s                      # Whitespace
+        (?P<year1>\d\d\d\d)     # Four digits
+        \s                      # Whitespace
+        et                      # The 'Et' word
+        \s                      # Whitespace
+        (?P<year2>\d\d\d\d)     # Four digits
+        """, re.UNICODE + re.X)
+    orDatePattern = re.compile("""
+        (?P<year1>\d\d\d\d)
+        \sou\s
+        (?P<year2>\d\d\d\d)
+        """, re.UNICODE + re.X)
+    decadeDatePattern = re.compile("""
+        Ann\wes                 # The 'Années' word
+        \s                      # Whitespace
+        (?P<year>\d\d\d\d)      # Four digits
+        """, re.UNICODE + re.X)
+    centuryPattern = re.compile("""
+        (?P<qualifier>Fin)?\s?  # The 'Fin' word, possibly
+        (?P<century>\d\d)       # Two digits
+        e                       # The 'e' letter
+        \s                      # Whitespace
+        si\wcle                 # The 'Siècle' word
+        """, re.UNICODE + re.X)
+
+    fullDateR = re.search(fullDatePattern, text)
+    monthDateR = re.search(monthDatePattern, text)
+    circaYearR = re.search(circaYearPattern, text)
+    circaDateR = re.search(circaDatePattern, text)
+    betweenDateR = re.search(betweenDatePattern, text)
+    orDateR = re.search(orDatePattern, text)
+    decadeDateR = re.search(decadeDatePattern, text)
+    centuryR = re.search(centuryPattern, text)
+
+    if betweenDateR:
+        print "betweenDateR"
+        date = u'{{Other date|between|%s|%s}}' % (betweenDateR.group('year1'),
+                                                  betweenDateR.group('year2'))
+        return (date, None)
+
+    elif orDateR:
+        print "orDateR"
+        date = u'{{Other date|or|%s|%s}}' % (orDateR.group('year1'),
+                                             orDateR.group('year2'))
+        return (date, None)
+
+    elif decadeDateR:
+        print "decadeDateR"
+        date = u'{{Other date|decade|%s}}' % (decadeDateR.group('year'))
+        return (date, None)
+
+    elif fullDateR:
+        print "fullDateR"
+        month = fullDateR.group('month').lower()
+        if month in monthList.keys():
+            print "%s is a key" % month
+            monthNum = monthList[month]
+            year = fullDateR.group('year')
+            date = u'%s-%s-%s' % (year,
+                                  '%02d' % monthList[month],
+                                  '%02d' % int(fullDateR.group('day')))
+            dateCategory = u"%s in " % fullDateR.group('year')
+            return (date, year)
+        else:
+            return (None, None)
+
+    elif circaDateR:
+        print "circaDateR"
+        month = circaDateR.group('month').lower()
+        if month in monthList.keys():
+            year = circaDateR.group('year')
+            date = u'{{Other date|circa|%s-%s}}' % (year,
+                                                    '%02d' % monthList[month])
+            return (date, year)
+
+    elif circaYearR:
+        print "circaYearR"
+        circaYear = circaYearR.group('year')
+        date = u'{{Other date|circa|%s}}' % (circaYear)
+        return (date, circaYear)
+
+    elif monthDateR:
+        print "monthDateR"
+        month = monthDateR.group('month').lower()
+        if month in monthList.keys():
+            year = monthDateR.group('year')
+            date = u'%s-%s' % (year, '%02d' % monthList[month])
+            dateCategory = u"%s in " % monthDateR.group('year')
+            return (date, year)
+        else:
+            return (None, None)
+
+    elif centuryR:
+        print "centuryR"
+        century = centuryR.group('century')
+        print centuryR.groups()
+        date = '{{Other date|century|%s}}' % (century)
+
+        if centuryR.groupdict()['qualifier']:
+            qualifier = centuryR.group('qualifier').lower()
+            table = {'fin': 'end'}
+            date = u'{{Other date|%s|%s}}' % (table[qualifier], date)
+        return (date, None)
+
+    else:
+        print "Nothing found"
+        print text
+        return (None, None)
--- test/test_processors.py
+++ test/test_processors.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import unittest
-from processors import parse_format_unwrapped
+from processors import parse_format_unwrapped, look_for_date_unwrapped
 
 
 class TestProcessors(unittest.TestCase):
@@ -13,3 +15,42 @@ class TestProcessors(unittest.TestCase):
         ]
         for value, expected in values:
             self.assertEqual(parse_format_unwrapped(value), expected)
+
+    def test_look_for_date(self):
+        values = [
+            ("Gare Matabiau, quai et voie. 4 octobre 1899. Vue d'ensemble",
+             ('1899-10-04', '1899')),  # FullDateR
+
+            ("Café Albrighi. Juillet 1905. Vue du café prise",
+             ('1905-07', '1905')),  # MonthDateR
+
+            ("Place des Carmes, côté nord. Vers 1905. Vue ",
+             ('{{Other date|circa|1905}}', '1905')),  # circaYearR
+
+            ("Place des Carmes, côté nord. Vers juillet 1905.Vue perspective",
+             ('{{Other date|circa|1905-07}}', '1905')),  # circaDateR
+
+            ("toulousaine. Entre 1888 et 1907. Vue de face d'une automobile",
+             ('{{Other date|between|1888|1907}}', None)),  # betweenDateR
+
+            (u"Entre les années 1859 et 1907. Vue d'une nature morte",
+             ('{{Other date|between|1859|1907}}', None)),  # betweenDateR bis
+
+            ("Joseph Delmas en costume d’Adam. Atelier Vidal. 1861 ou 1862",
+             ('{{Other date|or|1861|1862}}', None)),  # orDateR
+
+            (u"Jules-Guesde. Années 1890. Vue d'ensemble en",
+             ('{{Other date|decade|1890}}', None)),  # decadeDateR
+
+            (u"maritime escarpée. 19e siècle. Vue d'ensemble",
+             ('{{Other date|century|19}}', None)),  # centuryR
+
+            (u"Carcassonne (Aude). Fin 19e siècle. Vue d'ensemble",
+             ("{{Other date|end|{{Other date|century|19}}}}", None)),  # centuryR bis
+
+            ("Villeneuve les Avignon. Tour de Philippe le Bel. 1897",
+             (None, None)),  # Regular year - nothing
+        ]
+
+        for value, expected in values:
+            self.assertEqual(look_for_date_unwrapped(value), expected)
